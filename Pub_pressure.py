988ef958bbfbe157d9efc6e8e1485cc948920620
MarioG <mgm.projects23@gmail.com>
2022-11-12 19:33:57 +0100

    Pub Pressure - timer Pub NoiseLevel Pub Passenger Ok with the passenger

--- Pub_pressure.py
+++ Pub_pressure.py
@@ -8,7 +8,7 @@ from paho.mqtt import client as mqtt_client
 
 broker = 'localhost'
 port = 1883
-topic = "plane/temperature"
+topic = "plane/pressure"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
@@ -21,7 +21,7 @@ def connect_mqtt():
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client(client_id)  #1
     #client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -31,17 +31,39 @@ def connect_mqtt():
 def publish(client):
     msg_count = 0
     while True:
-        time.sleep(1)
+        msg_count += 1
+
         msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
+
+        while msg_count < 16:
+            time.sleep(1)
+            msg1 = random.randrange(98, 102, 1)
+            msg2 = random.randrange(99, 101)
+            msg3 = random.randrange(msg2-1, msg2+1)
+            client.publish("plane/pressure", msg_count)
+            msg_count += 1
+            #result = client.publish(topic, msg)
+        while 15 < msg_count < 31:
+            time.sleep(1)
+            client.publish("plane/pressure", "msg_count")
+            msg_count += 1
+
+        while 30 < msg_count < 46:
+
         # result: [0, 1]
+            time.sleep(1)
+            client.publish("plane/pressure", msg_count)
+            msg_count += 1
+
+        client.disconnect()
+"""
         status = result[0]
         if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
+            print(f"Send `{msg}` kPa to topic `{topic}`")
         else:
             print(f"Failed to send message to topic {topic}")
         msg_count += 1
-
+        """
 
 def run():
     client = connect_mqtt()
@@ -51,3 +73,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
