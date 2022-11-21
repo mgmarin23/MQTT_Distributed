26b417467853b95b28e7eda962be23f0c058c7dd
MarioG <mgm.projects23@gmail.com>
2022-11-21 21:06:31 +0100

    All

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -15,4 +17,4 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-Pub_pressure_int.run
+
